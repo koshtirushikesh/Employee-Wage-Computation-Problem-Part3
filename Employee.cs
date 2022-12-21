efbcf76715580403b78525cc6e3d91b07abd5ad0
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-21 18:34:55 +0530

    Refractor: using Switch Case Statement

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem3
+{
+    public class Employee
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public void SwitchCase()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int EMP_RATE_PER_HOUR = 20;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME: empHrs = 8; break;
+                case IS_PART_TIME: empHrs = 4; break;
+
+                default: empHrs = 0; break;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage: " + empWage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProblem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emplaye= new Employee();
+            emplaye.SwitchCase();
+        }
+    }
+}

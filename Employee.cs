8c39f9c46fd6824f74a365ff0f52874770912c84
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-21 17:49:48 +0530

    Check The Attendence

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem3
+{
+    public class Employee
+    {
+        public void Attendence()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
